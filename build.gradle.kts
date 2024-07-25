2b52110be4a6225ecb959c00b34aef19b6e5c9f0
Fırat Batar <firat.o.batar@gmail.com>
2024-07-25 14:24:30 +0300

    Add kotlinter

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
 	id("org.springframework.boot") version "3.3.2"
 	id("io.spring.dependency-management") version "1.1.6"
+	id("org.jmailen.kotlinter") version "4.1.0"
 	id("org.jetbrains.kotlin.jvm") version "1.9.24"
 	id("org.jetbrains.kotlin.plugin.spring") version "1.9.24"
 }
