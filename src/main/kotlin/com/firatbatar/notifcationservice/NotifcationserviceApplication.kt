254f1bdb689829f3ddd744a108fe82c83f3036a3
Fırat Batar <firat.o.batar@gmail.com>
2024-07-25 14:53:44 +0300

    [feature/add-pipeline] Add CI/CD pipeline

--- src/main/kotlin/com/firatbatar/notifcationservice/NotifcationserviceApplication.kt
+++ src/main/kotlin/com/firatbatar/notifcationservice/NotifcationserviceApplication.kt
@@ -7,5 +7,5 @@ import org.springframework.boot.runApplication
 class NotifcationserviceApplication
 
 fun main(args: Array<String>) {
-	runApplication<NotifcationserviceApplication>(*args)
+    runApplication<NotifcationserviceApplication>(*args)
 }
