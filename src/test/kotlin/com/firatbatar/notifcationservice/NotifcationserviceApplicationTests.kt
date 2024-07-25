b3bc279ccbe6b49ad9af6b63946ce204f05a447a
Fırat Batar <firat.o.batar@gmail.com>
2024-07-25 14:05:59 +0300

    Initialize Spring Boot

---
+++ src/test/kotlin/com/firatbatar/notifcationservice/NotifcationserviceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.firatbatar.notifcationservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NotifcationserviceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
