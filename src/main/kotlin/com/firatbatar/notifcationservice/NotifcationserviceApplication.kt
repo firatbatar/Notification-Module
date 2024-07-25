25c0febd0966780136123423af1df7a2a7c801e4
Fırat Batar <firat.o.batar@gmail.com>
2024-07-25 14:11:03 +0300

    Initialize Spring Boot

---
+++ src/main/kotlin/com/firatbatar/notifcationservice/NotifcationserviceApplication.kt
@@ -0,0 +1,11 @@
+package com.firatbatar.notifcationservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NotifcationserviceApplication
+
+fun main(args: Array<String>) {
+	runApplication<NotifcationserviceApplication>(*args)
+}
